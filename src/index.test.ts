25b0f919a887c7b0388f2caf931e51a762413a78
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-07-07 16:53:32 +0700

    Refactor tests for sum_number function and update GET /todo test case

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,18 +2,21 @@ import { describe, it, expect } from "vitest";
 import app from "./index.ts";
 import request from "supertest";
 
-describe("math tests", () => {
-  it("adds 1 + 2 to equal 3", () => {
-    expect(1 + 2).toBe(3);
+function sum_number(a: number, b: number) {
+  return a + b;
+}
+
+describe("Test sum_number function", () => {
+  it("should add two numbers correctly", () => {
+    const result = sum_number(1, 2);
+    expect(result).toBe(3);
   });
 });
 
 describe("GET /todo", () => {
-  it("should create a user when valid payload is passed", async () => {
+  it("should return all todos", async () => {
     const res = await request(app).get("/todo").send();
-
-    expect(res.status).toBe(201);
-    expect(res.body).toHaveProperty("id");
-    expect(res.body.email).toBe("test@example.com");
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
   });
 });
